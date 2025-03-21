2015c3fb0e29b8244e28aa1a51868c4d81eb127e
Hendrix Ejegi <tosanwumiae@gmail.com>
2025-03-21 21:44:19 +0100

    Add slick-carousel for improved carousel functionality and create educationPost data for solar energy articles

---
+++ src/data/educationPost.js
@@ -0,0 +1,53 @@
+const educationPost = [
+  {
+    id: 0,
+    title: "How Much Can You Save with Solar Energy?",
+    image: "./src/assets/images/save-with-solar.png",
+    summary: "t",
+    link: "#"
+  },
+  {
+    id: 1,
+    title: 'What’s the Right Solar Panel for Your Home?',
+    image: './src/assets/images/solar-home.png',
+    summary: 'Not all solar panels are the same! Monocrystalline, Polycrystalline, or Thin Film—which one suits your needs best?',
+    link: '#'
+  },
+  {
+    id: 2,
+    title: 'What’s the Right Solar Panel for Your Home?',
+    image: './src/assets/images/solar-home.png',
+    summary: 'Not all solar panels are the same! Monocrystalline, Polycrystalline, or Thin Film—which one suits your needs best?',
+    link: '#'
+  },
+  {
+    id: 3,
+    title: 'What’s the Right Solar Panel for Your Home?',
+    image: './src/assets/images/solar-home.png',
+    summary: 'Not all solar panels are the same! Monocrystalline, Polycrystalline, or Thin Film—which one suits your needs best?',
+    link: '#'
+  },
+  {
+    id: 4,
+    title: 'What’s the Right Solar Panel for Your Home?',
+    image: './src/assets/images/solar-home.png',
+    summary: 'Not all solar panels are the same! Monocrystalline, Polycrystalline, or Thin Film—which one suits your needs best?',
+    link: '#'
+  },
+  {
+    id: 5,
+    title: 'What’s the Right Solar Panel for Your Home?',
+    image: './src/assets/images/solar-home.png',
+    summary: 'Not all solar panels are the same! Monocrystalline, Polycrystalline, or Thin Film—which one suits your needs best?',
+    link: '#'
+  },
+  {
+    id: 6,
+    title: 'What’s the Right Solar Panel for Your Home?',
+    image: './src/assets/images/solar-home.png',
+    summary: 'Not all solar panels are the same! Monocrystalline, Polycrystalline, or Thin Film—which one suits your needs best?',
+    link: '#'
+  },
+];
+
+export default educationPost;
