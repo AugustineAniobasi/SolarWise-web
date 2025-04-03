094e5d181028de61d8e3f19392f37b42f4ed67b8
Hendrix Ejegi <tosanwumiae@gmail.com>
2025-04-03 17:52:09 +0100

    Add 404 Not Found page with navigation link to home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 import Banner from "./components/BannerComponent";
 import Header from "./components/HeaderComponent";
 import Footer from "./components/FooterComponent";
@@ -22,6 +22,24 @@ function RootLayout() {
   );
 }
 
+// 404 Page not found
+
+function NotFound() {
+  return (
+    <>
+      <div className="flex h-screen flex-col items-center justify-center">
+        <h1 className="text-center text-4xl font-bold">404 | Page not found</h1>
+        <Link
+          to="/"
+          className="bg-primary-500 mt-4 block w-fit rounded-lg p-[0.5rem_1rem] text-white"
+        >
+          Go Home
+        </Link>
+      </div>
+    </>
+  );
+}
+
 function App() {
   return (
     <>
@@ -32,6 +50,7 @@ function App() {
           <Route path="/solar-hub" element={<SolarHub />} />
           <Route path="/assessment-tool" element={<AssessmentTool />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
