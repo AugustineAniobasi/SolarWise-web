9c7162bd2448f639ca687ddf514db690b9526e46
Hendrix Ejegi <tosanwumiae@gmail.com>
2025-04-10 12:34:50 +0100

    Refactor: Replace smp-appl.js with appliances.js and update AssessmentTool component to use new data structure

---
+++ frontend/src/data/appliances.js
@@ -0,0 +1,9 @@
+const appliances = [
+  {id: 0, name: 'appliance', load: 30, isEnabled: true},
+  {id: 1, name: 'appliance', load: 30, isEnabled: true},
+  {id: 2, name: 'appliance', load: 30, isEnabled: true},
+  {id: 3, name: 'appliance', load: 30, isEnabled: true},
+  
+]
+
+export default appliances
--- frontend/src/pages/AssessmentToolPage/AssessmentTool.jsx
+++ frontend/src/pages/AssessmentToolPage/AssessmentTool.jsx
@@ -1,7 +1,20 @@
-import appliances from "@/data/smp-appl";
+import appliances from "@/data/appliances";
 import "./AssessmentTool.css";
+import { useState } from "react";
 
 export default function AssessmentTool() {
+  const [applianceData, setApplianceData] = useState(appliances);
+
+  function enableAppliance(id) {
+    setApplianceData((prevData) =>
+      prevData.map((appliance) =>
+        appliance.id === id
+          ? { ...appliance, isEnabled: !appliance.isEnabled }
+          : appliance,
+      ),
+    );
+  }
+
   return (
     <>
       <div className="assessment-hero flex h-[500px] items-end">
@@ -12,38 +25,57 @@ export default function AssessmentTool() {
       </div>
 
       <div className="calculator-container relative bg-[url(./src/assets/images/bg-vector.png)] bg-cover bg-center bg-no-repeat py-24">
-        <div className="absolute top-0 left-0 z-1 h-full w-full bg-white opacity-70"></div>
+        <div className="absolute top-0 left-0 z-1 h-full w-full bg-white opacity-80"></div>
+
         <div className="relative z-2">
           <p className="text-primary-500 m-auto mb-[70px] max-w-[900px] text-center text-[44px] font-normal">
             Calculate the amount of power your appliance would require
           </p>
+
           <div className="m-auto w-full max-w-[1046px]">
             <div className="appliance-table-header bg-primary-500 flex w-full items-center justify-between rounded-lg font-bold text-white">
               <span>Appliance</span>
               <span>Quantity</span>
               <span>Load</span>
             </div>
+
             <div className="appliance-table-body mt-11 flex w-full flex-col gap-4">
-              {appliances.map((appliance) => (
+              {applianceData.map((appliance) => (
                 <div
                   key={appliance.id}
                   className="appliance-table-row flex justify-between"
                 >
-                  <div>{appliance.name}</div>
-                  <div>Amount of Appliances?</div>
-                  <div>Amount of Power (W)</div>
+                  <button
+                    className="border-primary-400 hover:ring-primary-300 cursor-pointer border-2 font-bold hover:ring-2"
+                    onClick={() => enableAppliance(appliance.id)}
+                  >
+                    {appliance.name}
+                  </button>
+                  <input
+                    type="number"
+                    placeholder="Amount of Appliances?"
+                    disabled={appliance.isEnabled}
+                  />
+                  <div>
+                    {!appliance.isEnabled
+                      ? appliance.load
+                      : "Amount of Power (W)"}
+                  </div>
                 </div>
               ))}
             </div>
+
             <div className="appliance-table-footer flex w-full gap-6">
               <button>Add New Appliance</button>
               <button>Calculate Power</button>
             </div>
           </div>
-          <div className="bg-primary-500 m-auto mt-[5rem] w-full max-w-[997px] px-12 py-[80px]">
+
+          <div className="bg-primary-500 m-auto mt-[5rem] w-full max-w-[997px] rounded-lg px-12 py-[80px]">
             <p className="text-center text-[2rem] text-white">
               Your result from the calculation
             </p>
+
             <div className="mt-4 flex gap-10">
               <div className="calculation-result">
                 <div>
